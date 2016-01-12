0488deb565f8b2c2829abdd538bef15e0b080659
Danial Javanamrdi <sunny_javanmardi@yahoo.com>
2016-01-11 22:03:10 -0500

    the Visualization3 version2

--- view2/diagram3.js
+++ view2/diagram3.js
@@ -1,6 +1,6 @@
 // Dimensions of sunburst.
-var width = 750;
-var height = 640;
+var width = 950;
+var height = 580;
 
 var radius = 630;
 
@@ -8,7 +8,7 @@ var radius = 630;
 var b = {
   w: 175, h: 30, s: 3, t: 10
 };
-var circleCenter = {x:((width-120 ) / 2)-20, y:((height-100) / 2)+20};
+var circleCenter = {x:295, y:290};
   
 // Mapping of step names to colors.
 var colors = {
@@ -51,6 +51,16 @@ var arc = d3.svg.arc()
 
 var json, csv;
 var root;
+var path;
+
+var percentageData = [];
+d3.json("data3_percentage.php", function(error, data) {
+        //get the data from Database - PHP 
+        data.forEach(function(d){
+            percentageData.push(d);
+        });
+});
+
 var givenData = []; 
 d3.json("data3.php", function(error, data) {
         //get the data from Database - PHP 
@@ -60,6 +70,7 @@ d3.json("data3.php", function(error, data) {
         json = buildHierarchy(givenData);
         createVisualization(json);
 });
+ 
 
 // Main function to draw and set up the visualization, once we have the data.
 function createVisualization(json) {
@@ -79,19 +90,25 @@ function createVisualization(json) {
       return (d.dx > 0.005); // 0.005 radians = 0.29 degrees
       });
 
-  var path = vis.data([json]).selectAll("path")
+   path = vis.data([json]).selectAll("path")
       .data(nodes)
       .enter().append("svg:path")
       .attr("display", function(d) { return d.depth ? null : "none"; })
       .attr("d", arc)
       .attr("fill-rule", "evenodd")
       .style("fill", "white" )
-      .style("fill", function(d,i) { return color_random(d.name.replace("Wikipedia:","")); //colors[d.name]; 
+      .style("fill", function(d,i) {
+        return color_random(d.name.replace("Wikipedia:","")); //colors[d.name];
       })
       .style("opacity", 1)
       .style("cursor", "pointer")
+      .attr("id",function(d){
+            return d.depth+"_"+d.name.replace(':','') ;
+        })
       //.on("click", enableCenter)
       .on("mouseenter", enableCenter);
+      
+     
   var limitTextDisplay = 500;
   var radiusD = radius -400;
   var adjustR = { r1:radiusD-160 , r2:radiusD-70  , r3:radiusD-20 , r4:radiusD };
@@ -114,64 +131,87 @@ function createVisualization(json) {
                         })
                         .attr("y", function(d) { return d.cy; })
                         .text(function (d) {
-                            if(+d.depth==1) 
-                                return d.name.replace("Wikipedia:","");
-                            if((+d.depth== 3 && +d.value > 800) || (+d.depth== 2 && +d.value > 1600))
-                                return d.name.replace("Wikipedia:","");
-                            //if(+d.depth>0) return d.delete_;
+                                return d.name;
                          })
+                        .style("opacity",function(d){
+                            if(+d.depth==1) 
+                                return 1; 
+                            else if((+d.depth== 3 && +d.value > 800) || (+d.depth== 2 && +d.value > 1600))
+                                return 1;
+                            else
+                                return 0.01;
+                        })
                         .attr("transform", function (d) {
                             var r = 180 * ((d.x + d.dx / 2 - Math.PI / 2) / Math.PI);
                             return "rotate(" + r + ")"
-                        });
-                        
+                        })
+                        .on("mouseenter", enableCenter);
       
   // Add the mouseleave handler to the bounding circle.
-  d3.select("#container").on("mouseleave", disableCenter);
+  d3.select("#chart").on("mouseleave", disableCenter);
   //d3.select("#explanation").on("click", disableCenter);
    
   // Get total size of the tree = value of root node from partition.
   totalSize = path.node().__data__.value;
+  
+  setTimeout(setExtraAttributes, 500)
+  //set all extra attributes to the arces
  };
-
-function showTitle(d) {
-    d3.select("#titleHover")
-      .text(d.name);
-}
 
 // Fade all but the current sequence, and show it in the breadcrumb trail.
 function enableCenter(d) {
-
-  var percentage = (100 * d.value / totalSize).toPrecision(3);
-  var label = d.name ;
-  var percentageString = percentage + "%";
-  if (percentage < 0.1) {
-    percentageString = "< 0.1%";
-  }
-
-  d3.select("#title")
-      //.text(label.replace("Wikipedia:",""));
-      .text(label);
-  
-  d3.select("#percentage")
-      .text( percentageString);
-
-  d3.select("#explanation")
-      .style("visibility", "")
-
-  var sequenceArray = getAncestors(d);
-  updateBreadcrumbs(sequenceArray, percentageString);
-
-  // Fade all the segments.
-  d3.selectAll("path")
-      .style("opacity", 0.3);
-
-  // Then highlight only those that are an ancestor of the current segment.
-  vis.selectAll("path")
-      .filter(function(node) {
-                return (sequenceArray.indexOf(node) >= 0);
-              })
-      .style("opacity", 1);
+  //in case of delay from database, this is to double check
+  setExtraAttributes();  
+  if(isSet)
+  {
+      var percentage = (100 * d.value / totalSize).toPrecision(3);
+      var label = d.name ;
+      var percentageString = percentage + "%";
+      if (percentage < 0.1) {
+        percentageString = "< 0.1%";
+      }
+    
+      d3.select("#title")
+          //.text(label.replace("Wikipedia:",""));
+          .text(label);
+      
+      d3.select("#percentage")
+          .text( percentageString);
+    
+      d3.select("#explanation")
+          .style("visibility", "")
+    
+      var sequenceArray = getAncestors(d);
+      updateBreadcrumbs(sequenceArray, percentageString);
+    
+      // Fade all the segments.
+      d3.selectAll("path")
+          .style("opacity", 0.2);
+      d3.selectAll(".pathlabel")
+        .style("opacity",function(d){
+                            if(+d.depth==1) 
+                                return 0.2 
+                            else if((+d.depth== 3 && +d.value > 800) || (+d.depth== 2 && +d.value > 1600))
+                                return 0.2;
+                            else
+                                return 0.01;
+                        })
+    
+      // Then highlight only those that are an ancestor of the current segment.
+      vis.selectAll("path")
+          .filter(function(node) {
+                    return (sequenceArray.indexOf(node) >= 0);
+                  })
+          .style("opacity", 1);
+      vis.selectAll(".pathlabel")
+          .filter(function(node) {
+                    return (sequenceArray.indexOf(node) >= 0);
+                  })
+          .style("opacity", 1)
+          .style("fill", "#111");
+    
+      displayBarChar(d);
+    }      
 }
 
 function disableCenter(d){
@@ -187,6 +227,19 @@ function disableCenter(d){
       .transition()
       .duration(1000)
       .style("opacity", 1);
+      
+  d3.selectAll(".pathlabel")
+      .transition()
+      .duration(1000)
+        .style("opacity",function(d){
+                            if(+d.depth==1) 
+                                return 1; 
+                            else if((+d.depth== 3 && +d.value > 800) || (+d.depth== 2 && +d.value > 1600))
+                                return 1;
+                            else
+                                return 0.01;
+                        })
+      .style("fill", "#666");
 
   d3.select("#explanation")
       .style("visibility", "hidden");
@@ -205,6 +258,7 @@ function getAncestors(node) {
 }
 
 function initializeBreadcrumbTrail() {
+    
   // Add the svg area.
   var trail = d3.select("#sequence").append("svg:svg")
       .attr("width", width)
@@ -215,14 +269,16 @@ function initializeBreadcrumbTrail() {
     .attr("id", "endlabel")
     .style("fill", "#000");
 }
+var charToPix = 6.4;
+var charPadding = 70;
 
 // Generate a string that describes the points of a breadcrumb polygon.
 function breadcrumbPoints(d, i) {
   var points = [];
   points.push("0,0");
-  points.push(b.w + ",0");
-  points.push(b.w + b.t + "," + (b.h / 2));
-  points.push(b.w + "," + b.h);
+  points.push(d.policyTitle.length*charToPix+charPadding + ",0");
+  points.push(d.policyTitle.length*charToPix+charPadding + b.t + "," + (b.h / 2));
+  points.push(d.policyTitle.length*charToPix+charPadding + "," + b.h);
   points.push("0," + b.h);
   if (i > 0) { // Leftmost breadcrumb; don't include 6th vertex.
     points.push(b.t + "," + (b.h / 2));
@@ -246,32 +302,26 @@ function updateBreadcrumbs(nodeArray, percentageString) {
       .style("fill", function(d,i) { return color_random(d.name.replace("Wikipedia:","")); //colors[d.name]; 
       })
   entering.append("svg:text")
-      .attr("x", (b.w + b.t) / 2)
+      .attr("x",function(d){ return (d.policyTitle.length*charToPix+charPadding + b.t) / 2;  })
       .attr("y", b.h / 2)
       .attr("dy", "0.35em")
       .attr("text-anchor", "middle")
-      .text(function(d) { return d.name ;  });
+      .text(function(d) { return ""+(+d.percentage_otherComment*100).toFixed(2)+"% - " + d.policyTitle ;  });
 
   // Set position for entering and updating nodes.
   g.attr("transform", function(d, i) {
-    return "translate(" + i * (b.w + b.s) + ", 0)";
+    var xShift = 0;
+    for (var j = 0; j < i; j++) {
+        xShift += nodeArray[j].policyTitle.length*charToPix+charPadding + b.s;
+    }
+    return "translate(" + xShift + ", 0)";
   });
-
   // Remove exiting nodes.
   g.exit().remove();
-
-  // Now move and update the percentage at the end.
-  d3.select("#trail").select("#endlabel")
-      .attr("x", (nodeArray.length + 0.5) * (b.w + b.s))
-      .attr("y", b.h / 2)
-      .attr("dy", "0.35em")
-      .attr("text-anchor", "middle")
-      .text(percentageString);
-
+  
   // Make the breadcrumb trail visible, if it's hidden.
   d3.select("#trail")
       .style("visibility", "");
-
 }
 
 // Take a 2-column CSV and transform it into a hierarchical structure suitable
@@ -307,20 +357,86 @@ function buildHierarchy(csv) {
              	}
               // If we don't already have a child node for this branch, create it.
              	if (!foundChild) {
-             	  
-             	  childNode = {"name": nodeName, "children": []};
-             	  children.push(childNode);
+             	      childNode = {"name": nodeName, "children": []};
+             	      children.push(childNode);
              	}
              	currentNode = childNode;
           } else {
              	// Reached the end of the sequence; create a leaf node.
-             	childNode = {"name": nodeName, "size": size, "title": title, "delete_": delete_, "other_": other_, "keep_": keep_ };
-             	children.push(childNode);
+             	      childNode = {"name": nodeName, "size": size, "title": title, "delete_": delete_, "other_": other_, "keep_": keep_ };
+             	      children.push(childNode);
           }
     }
-    //added by lili 
+    
   }
   return root;
 };
+
+
+// in the case percentageData has been not loaded yet
+var isSet = false;
+function setExtraAttributes()
+{
+    if(!isSet)
+    {
+        path.each(function(d,i){
+            //console.log(d.name+"@"+d.depth);
+            for (var i = 0; i < percentageData.length; i++) {
+                isSet = true;
+                if( percentageData[i]["tier"] == +d.depth)
+                     if( percentageData[i]["policyID"] == d.name)
+                     {
+                        d["policyTitle"] = percentageData[i]["policyTitle"];
+                        d["policyURL"] = percentageData[i]["policyURL"];
+                        d["percentage_otherComment"] = percentageData[i]["percentage_otherComment"];
+                        d["percentage_delete"] = percentageData[i]["percentage_delete"];
+                        d["percentage_other"] = percentageData[i]["percentage_other"];
+                        d["percentage_keep"] = percentageData[i]["percentage_keep"];
+                     }
+            }
+        });
+    }
+}
+
+// Added 
+function displayBarChar(d)
+{
+    var barCharts;
+    var percentageScale = d3.scale.linear()
+        .domain([0.0,100])
+        .range([0.5,300]);
+        
+    var rectData = [    {percentage_delete: (+d.percentage_delete*100) ,
+                         percentage_other: "15.0",
+                         percentage_keep: "30.0"},
+                        {percentage_delete: (+d.percentage_other*100),
+                         percentage_other: "10.0",
+                         percentage_keep: "45.0"},
+                        {percentage_delete: (+d.percentage_keep*100),
+                         percentage_other: "10.0",
+                         percentage_keep: "1.0"}];
+    var distance = 20;
+    var baseX = 300 ;
+    var baseY = -250;
+    vis.selectAll(".barChart").remove();
+    barCharts = vis.selectAll(".barChart")
+                .data(rectData)
+                .enter()
+                .append("rect")
+                .attr("class", "barChart")
+                .attr("id", function (d){ return "rect_"; })
+                .style("fill", "red")
+                .attr("x", function(d, i ) {return baseX  ;})
+                .attr("y", function(d, i ) {return baseY + 5 + distance*i; })
+                .attr("height", function (d){ return 10; })
+                .attr("width", function (d){return  percentageScale(+(d.percentage_delete));});
+    
+    /*vis.append("text")
+        .text("Delete votes")
+        .attr("x", 300    )
+        .attr("y", -250  );*/    
+}
+
+
 
 
